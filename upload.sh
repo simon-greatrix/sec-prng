8845896a88e3037319ff86588e7400933cd42266
Simon Greatrix <simon.greatrix@eeze.com>
2025-04-10 13:24:52 +0100

    Added note at end of upload

--- upload.sh
+++ upload.sh
@@ -5,12 +5,12 @@ source ~/.gradle/central.sh
 
 BEARER=`printf "$CENTRAL_USERNAME_TOKEN:$CENTRAL_PASSWORD_TOKEN" | base64`
 
-# curl \
-#   --request POST \
-#   --verbose \
-#   --header "Authorization: Bearer $BEARER" \
-#   --form bundle=@core/build/publications/central.zip \
-#   https://central.sonatype.com/api/v1/publisher/upload
+ curl \
+   --request POST \
+   --verbose \
+   --header "Authorization: Bearer $BEARER" \
+   --form bundle=@core/build/publications/central.zip \
+   https://central.sonatype.com/api/v1/publisher/upload
 
 curl \
   --request POST \
@@ -25,3 +25,7 @@ curl \
   --header "Authorization: Bearer $BEARER" \
   --form bundle=@utilities/build/publications/central.zip \
   https://central.sonatype.com/api/v1/publisher/upload
+
+
+echo "Now login to https://central.sonatype.com/ and publish the zips"
+
